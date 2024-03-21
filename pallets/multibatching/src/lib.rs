87d5cbf77661220aafcd94db8032efce26ac1460
Mikola Samardak <niksaak@gmail.com>
2024-03-21 00:54:51 +0000

    Implement the new interface

--- pallets/multibatching/src/lib.rs
+++ pallets/multibatching/src/lib.rs
@@ -1,8 +1,5 @@
 #![cfg_attr(not(feature = "std"), no_std)]
 
-/// Edit this file to define custom logic or remove it if it is not needed.
-/// Learn more about FRAME and the core library of Substrate FRAME pallets:
-/// <https://docs.substrate.io/reference/frame-pallets/>
 pub use pallet::*;
 
 #[cfg(test)]
@@ -16,194 +13,259 @@ mod benchmarking;
 
 #[frame_support::pallet]
 pub mod pallet {
-	use super::*;
-	use frame_support::{
-		dispatch::GetDispatchInfo,
-		pallet_prelude::*,
-		sp_runtime::traits::{Hash, IdentifyAccount, Verify},
-		traits::UnfilteredDispatchable,
-	};
-	use frame_system::pallet_prelude::*;
+    use super::*;
+    use frame_support::{
+        pallet_prelude::*,
+        dispatch::{extract_actual_weight, GetDispatchInfo, PostDispatchInfo},
+        traits::{UnfilteredDispatchable, IsSubType, OriginTrait},
+        sp_runtime::traits::{IdentifyAccount, Verify, Hash, Dispatchable},
+    };
+    use frame_system::{
+        pallet_prelude::*,
+        WeightInfo,
+    };
 
-	#[pallet::pallet]
-	pub struct Pallet<T>(_);
+    #[pallet::pallet]
+    pub struct Pallet<T>(_);
 
-	#[pallet::config]
-	pub trait Config: frame_system::Config {
-		type RuntimeEvent: From<Event<Self>> + IsType<<Self as frame_system::Config>::RuntimeEvent>;
+    #[pallet::config]
+    pub trait Config: frame_system::Config {
+        type RuntimeEvent: From<Event<Self>> + IsType<<Self as frame_system::Config>::RuntimeEvent>;
 
-		type RuntimeCall: Parameter
-			+ GetDispatchInfo
-			+ UnfilteredDispatchable<RuntimeOrigin = Self::RuntimeOrigin>;
+        type RuntimeCall:
+            Parameter
+            + GetDispatchInfo
+            + UnfilteredDispatchable<RuntimeOrigin = Self::RuntimeOrigin>
+            + Dispatchable<RuntimeOrigin = Self::RuntimeOrigin, PostInfo = PostDispatchInfo>
+            + From<frame_system::Call<Self>>
+            + IsSubType<Call<Self>>
+            + IsType<<Self as frame_system::Config>::RuntimeCall>;
 
-		type Signature: Verify<Signer = Self::Signer>
-			+ Clone
-			+ Encode
-			+ Decode
-			+ Member
-			+ Parameter
-			+ TypeInfo;
+        type Signature:
+            Verify<Signer = Self::Signer>
+            + Clone
+            + Encode
+            + Decode
+            + Member
+            + Parameter
+            + TypeInfo;
 
-		type Signer: IdentifyAccount<AccountId = Self::AccountId>
-			+ Clone
-			+ Encode
-			+ Decode
-			+ Parameter;
+        type Signer:
+            IdentifyAccount<AccountId = Self::AccountId>
+            + Clone
+            + Encode
+            + Decode
+            + Parameter;
 
-		#[pallet::constant]
-		type MaxCalls: Get<u32>;
-	}
+        // TODO: this should really be a u16, but the trait bound on BoundedVec
+        // requires Get<u32>, which is not implemented for ConstU16.
+        #[pallet::constant]
+        type MaxCalls: Get<u32>;
 
-	#[pallet::error]
-	pub enum Error<T> {
-		InvalidOrigin,
-		AlreadyApplied,
-		BatchSenderIsNotOrigin,
-		InvalidCallOrigin(u16),
-		InvalidSignature(u16),
-	}
+        type WeightInfo: WeightInfo;
+    }
 
-	#[pallet::storage]
-	#[pallet::getter(fn applied)]
-	pub type Applied<T: Config> = StorageMap<_, Identity, T::Hash, (), ValueQuery>;
+    #[pallet::error]
+    pub enum Error<T> {
+        AlreadyApplied,
+        BatchSenderIsNotOrigin,
+        NoCalls,
+        NoApprovals,
+        InvalidDomain,
+        DomainNotSet,
+        InvalidCallOrigin(u16),
+        InvalidSignature(u16),
+    }
 
-	#[pallet::event]
-	#[pallet::generate_deposit(pub(super) fn deposit_event)]
-	pub enum Event<T: Config> {
-		BatchApplied { hash: T::Hash },
-	}
+    #[pallet::storage]
+    #[pallet::getter(fn domain)]
+    pub type Domain<T: Config> = StorageValue<_, [u8;32], OptionQuery>;
 
-	/// A batch of calls.
-	///
-	/// Every participant that has a call in this batch must sign the hash
-	/// of a batch in full, including the public key of the signer.
-	///
-	/// `sender` must be the origin of the `batch` extrinsic.
-	#[derive(Encode, Decode, TypeInfo, MaxEncodedLen)]
-	#[scale_info(skip_type_params(T))]
-	pub struct Batch<T: Config> {
-		pub sender: T::AccountId,
-		pub bias: [u8; 32],
-		pub calls: BoundedVec<BatchedCall<T>, T::MaxCalls>,
-	}
+    #[pallet::storage]
+    #[pallet::getter(fn applied)]
+    pub type Applied<T: Config> = StorageMap<_, Identity, T::Hash, (), ValueQuery>;
 
-	// TODO: Is there a way to avoid manual impls here?
-	impl<T: Config> Clone for Batch<T> {
-		fn clone(&self) -> Self {
-			Self { sender: self.sender.clone(), bias: self.bias.clone(), calls: self.calls.clone() }
-		}
-	}
+    #[pallet::event]
+    #[pallet::generate_deposit(pub(super) fn deposit_event)]
+    pub enum Event<T: Config> {
+        BatchApplied {
+            hash: T::Hash
+        },
+    }
 
-	impl<T: Config> PartialEq for Batch<T> {
-		fn eq(&self, other: &Self) -> bool {
-			self.sender == other.sender && self.calls == other.calls
-		}
-	}
+    /// A batch of calls.
+    ///
+    /// Every participant that has a call in this batch must sign the hash
+    /// of a batch in full, including the public key of the signer.
+    ///
+    /// `sender` must be the origin of the `batch` extrinsic.
+    #[derive(Encode, Decode, TypeInfo, MaxEncodedLen)]
+    #[scale_info(skip_type_params(T))]
+    pub struct Batch<T: Config> {
+        pub pallet_index: u8,
+        pub call_index: u8,
+        pub domain: [u8; 32],
+        pub sender: T::AccountId,
+        pub bias: [u8; 32],
+        pub calls: BoundedVec<BatchedCall<T>, T::MaxCalls>,
+        pub approvals_zero: u8,
+    }
 
-	impl<T: Config> core::fmt::Debug for Batch<T> {
-		fn fmt(&self, f: &mut core::fmt::Formatter<'_>) -> Result<(), core::fmt::Error> {
-			f.debug_struct("Batch")
-				.field("sender", &self.sender)
-				.field("calls", &self.calls)
-				.finish()
-		}
-	}
+    impl<T: Config> PartialEq for Batch<T> {
+        fn eq(&self, other: &Self) -> bool {
+            self.sender == other.sender && self.calls == other.calls
+        }
+    }
 
-	/// A call in a batch.
-	#[derive(Clone, Encode, Decode, PartialEq, Eq, TypeInfo, MaxEncodedLen)]
-	#[scale_info(skip_type_params(T))]
-	pub struct BatchedCall<T: Config> {
-		/// The public key that will be the origin of this call.
-		pub from: T::Signer,
-		/// The runtime call.
-		pub call: <T as Config>::RuntimeCall,
-	}
+    // TODO: remove this before signoff
+    impl<T: Config> core::fmt::Debug for Batch<T> {
+        fn fmt(&self, f: &mut core::fmt::Formatter<'_>) -> Result<(), core::fmt::Error> {
+            f.debug_struct("Batch")
+                .field("sender", &self.sender)
+                .field("calls", &self.calls)
+                .finish()
+        }
+    }
 
-	/// A signature of a batch by one of its participants.
-	#[derive(Clone, Encode, Decode, PartialEq, TypeInfo, MaxEncodedLen)]
-	#[scale_info(skip_type_params(T))]
-	pub struct Approval<T: Config> {
-		pub from: T::Signer,
-		pub signature: T::Signature,
-	}
+    /// A call in a batch.
+    #[derive(Clone, Encode, Decode, PartialEq, Eq, TypeInfo, MaxEncodedLen)]
+    #[scale_info(skip_type_params(T))]
+    pub struct BatchedCall<T: Config> {
+        /// The public key that will be the origin of this call.
+        pub from: T::Signer,
+        /// The runtime call.
+        pub call: <T as Config>::RuntimeCall,
+    }
 
-	impl<T: Config> core::fmt::Debug for BatchedCall<T> {
-		fn fmt(&self, f: &mut core::fmt::Formatter<'_>) -> Result<(), core::fmt::Error> {
-			f.debug_struct("BatchedCall")
-				.field("from", &self.from)
-				.field("call", &"<call>")
-				.finish()
-		}
-	}
+    /// A signature of a batch by one of its participants.
+    #[derive(Clone, Encode, Decode, PartialEq, TypeInfo, MaxEncodedLen)]
+    #[scale_info(skip_type_params(T))]
+    pub struct Approval<T: Config> {
+        pub from: T::Signer,
+        pub signature: T::Signature,
+    }
 
-	impl<T: Config> core::fmt::Debug for Approval<T> {
-		fn fmt(&self, f: &mut core::fmt::Formatter<'_>) -> Result<(), core::fmt::Error> {
-			f.debug_struct("Approval")
-				.field("from", &self.from)
-				.field("signature", &self.signature)
-				.finish()
-		}
-	}
+    // TODO: remove this before signoff
+    impl<T: Config> core::fmt::Debug for BatchedCall<T> {
+        fn fmt(&self, f: &mut core::fmt::Formatter<'_>) -> Result<(), core::fmt::Error> {
+            f.debug_struct("BatchedCall")
+                .field("from", &self.from)
+                .field("call", &"<call>")
+                .finish()
+        }
+    }
 
-	#[pallet::call]
-	impl<T: Config> Pallet<T> {
-		/// Execute multiple calls from multiple senders in a batch atomically.
-		///
-		/// If one of the calls fails, the whole batch reverts. Every participant
-		/// of the batch must sign the hash of its Scale-encoded representation.
-		#[pallet::call_index(0)]
-		#[pallet::weight({0})] // TODO: weight
-		pub fn batch(
-			origin: OriginFor<T>,
-			batch: Batch<T>,
-			approvals: BoundedVec<Approval<T>, T::MaxCalls>,
-		) -> DispatchResult {
-			if batch.calls.len() > 0 && approvals.is_empty() {
-				return Err(Error::<T>::InvalidSignature(0).into());
-			}
+    // TODO: remove this before signoff
+    impl<T: Config> core::fmt::Debug for Approval<T> {
+        fn fmt(&self, f: &mut core::fmt::Formatter<'_>) -> Result<(), core::fmt::Error> {
+            f.debug_struct("Approval")
+                .field("from", &self.from)
+                .field("signature", &self.signature)
+                .finish()
+        }
+    }
 
-			match ensure_signed(origin) {
-				Ok(account_id) if account_id == batch.sender => (),
-				Ok(_) => return Err(Error::<T>::BatchSenderIsNotOrigin.into()),
-				Err(_) => return Err(Error::<T>::InvalidOrigin.into()),
-			}
+    #[pallet::call]
+    impl<T: Config> Pallet<T> {
+        /// Execute multiple calls from multiple senders in a batch atomically.
+        ///
+        /// If one of the calls fails, the whole batch reverts.
+        ///
+        /// For every unique call origin, `approvals` must contain a signature
+        /// of keccak256 hash of this call with empty `approvals` argument.
+        /// This is so that the participants could check the data they sign
+        /// through the Developer/Extrinsics/Decode tab on the parachain
+        /// explorer: https://polkadot.js.org/apps/#/extrinsics/decode
+        #[pallet::call_index(0)]
+        #[pallet::weight({0})] // TODO: weight
+        pub fn batch(
+            origin: OriginFor<T>,
+            domain: [u8; 32],
+            sender: T::AccountId,
+            bias: [u8; 32],
+            calls: BoundedVec<BatchedCall<T>, T::MaxCalls>,
+            approvals: BoundedVec<Approval<T>, T::MaxCalls>,
+        ) -> DispatchResultWithPostInfo {
+            if calls.is_empty() {
+                return Err(Error::<T>::NoCalls.into());
+            }
+            if approvals.is_empty() {
+                return Err(Error::<T>::NoApprovals.into());
+            }
 
-			let hash = {
-				let bytes = batch.encode();
-				<T::Hashing>::hash(&bytes)
-			};
+            match ensure_signed(origin) {
+                Ok(account_id) if account_id == sender => account_id,
+                Ok(_) => return Err(Error::<T>::BatchSenderIsNotOrigin.into()),
+                Err(e) => return Err(e.into()),
+            };
 
-			if Applied::<T>::contains_key(&hash) {
-				return Err(Error::<T>::AlreadyApplied.into());
-			}
+            match Domain::<T>::get() {
+                Some(stored_domain) if stored_domain == domain => (),
+                Some(_) => return Err(Error::<T>::InvalidDomain.into()),
+                None => return Err(Error::<T>::DomainNotSet.into()),
+            }
 
-			Applied::<T>::insert(hash, ());
+            let hash = {
+                let batch = Batch {
+                    pallet_index: Self::index() as u8,
+                    call_index: 0,
+                    domain,
+                    sender: sender.clone(),
+                    bias,
+                    calls: calls.clone(),
+                    approvals_zero: 0,
+                };
+                let bytes = batch.encode();
+                <T::Hashing>::hash(&bytes)
+            };
 
-			for (i, approval) in approvals.iter().enumerate() {
-				let ok =
-					approval.signature.verify(hash.as_ref(), &approval.from.clone().into_account());
-				if !ok {
-					return Err(Error::<T>::InvalidSignature(i as u16).into());
-				}
-			}
+            if Applied::<T>::contains_key(&hash) {
+                return Err(Error::<T>::AlreadyApplied.into());
+            }
 
-			// TODO: count weight of batched calls
+            Applied::<T>::insert(hash, ());
 
-			for (i, payload) in batch.calls.into_iter().enumerate() {
-				let ok = approvals.iter().any(|int| int.from == payload.from);
-				if !ok {
-					return Err(Error::<T>::InvalidCallOrigin(i as u16).into());
-				}
+            for (i, approval) in approvals.iter().enumerate() {
+                let ok = approval.signature.verify(hash.as_ref(), &approval.from.clone().into_account());
+                if !ok {
+                    return Err(Error::<T>::InvalidSignature(i as u16).into());
+                }
+            }
 
-				let origin = <T::RuntimeOrigin>::from(frame_system::RawOrigin::Signed(
-					payload.from.into_account(),
-				));
-				payload.call.dispatch_bypass_filter(origin).map_err(|e| e.error)?;
-			}
 
-			Self::deposit_event(Event::BatchApplied { hash });
+            let mut weight = Weight::zero();
 
-			Ok(())
-		}
-	}
+            for (i, payload) in calls.into_iter().enumerate() {
+                let ok = approvals
+                    .iter()
+                    .any(|int| int.from == payload.from);
+                if !ok {
+                    return Err(Error::<T>::InvalidCallOrigin(i as u16).into());
+                }
+
+                let info = payload.call.get_dispatch_info();
+                let mut origin = <T::RuntimeOrigin>::from(frame_system::RawOrigin::Signed(payload.from.into_account()));
+                origin.add_filter(move |c: &<T as frame_system::Config>::RuntimeCall| {
+                    let c = <T as Config>::RuntimeCall::from_ref(c);
+                    !matches!(c.is_sub_type(), Some(Call::batch { .. }))
+                });
+                let result = payload.call.dispatch(origin);
+                weight = weight.saturating_add(extract_actual_weight(&result, &info));
+				result.map_err(|mut err| {
+					// Take the weight of this function itself into account.
+					// let base_weight = T::WeightInfo::batch_all(i.saturating_add(1) as u32); // TODO
+                    let base_weight = Weight::zero();
+					// Return the actual used weight + base_weight of this call.
+					err.post_info = Some(base_weight + weight).into();
+					err
+				})?;
+            }
+
+            Self::deposit_event(Event::BatchApplied { hash });
+
+			//let base_weight = T::WeightInfo::batch_all(calls_len as u32); // TODO
+			let base_weight = Weight::zero();
+			Ok(Some(base_weight.saturating_add(weight)).into())
+        }
+    }
 }
