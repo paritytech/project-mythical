2f43ab77a79e5575b1d8f162eb118bb60b21c4e3
metricaez <emiliano@blockdeep.io>
2024-04-05 12:22:03 -0300

    feat: impl benchmark test suite, set next collection benchmark

--- pallets/migration/src/benchmarking.rs
+++ pallets/migration/src/benchmarking.rs
@@ -9,6 +9,14 @@ const SEED: u32 = 0;
 fn assert_last_event<T: Config>(generic_event: <T as Config>::RuntimeEvent) {
 	frame_system::Pallet::<T>::assert_last_event(generic_event.into());
 }
+
+fn get_migrator<T: Config>() -> T::AccountId {
+	let migrator: T::AccountId = account("migrator", 10, SEED);
+	whitelist_account!(migrator);
+	assert_ok!(Migration::<T>::force_set_migrator(RawOrigin::Root.into(), migrator.clone()));
+
+	migrator
+}
 #[benchmarks()]
 pub mod benchmarks {
 	use super::*;
@@ -23,5 +31,16 @@ pub mod benchmarks {
 		assert_last_event::<T>(Event::MigratorUpdated(migrator).into());
 	}
 
-    impl_benchmark_test_suite!(Migration, crate::mock::new_test_ext(), crate::mock::Test);
+	#[benchmark]
+	fn set_next_collection_id() {
+		let next_collection_id: <T as pallet_nfts::Config>::CollectionId = (1 as u32).into();
+		let migrator: T::AccountId = get_migrator::<T>();
+
+		#[extrinsic_call]
+		_(RawOrigin::Signed(migrator), next_collection_id.clone());
+
+		assert_last_event::<T>(Event::NextCollectionIdUpdated(next_collection_id).into());
+	}
+
+	impl_benchmark_test_suite!(Migration, crate::mock::new_test_ext(), crate::mock::Test);
 }
