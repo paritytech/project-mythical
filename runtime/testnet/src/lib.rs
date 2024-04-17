dbb824ce8d9b3124712026dfaa444a301b5e399f
Mikola Samardak <niksaak@gmail.com>
2024-04-17 14:47:33 +0100

    Increment spec version

--- runtime/testnet/src/lib.rs
+++ runtime/testnet/src/lib.rs
@@ -249,7 +249,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
 	spec_name: create_runtime_str!("muse"),
 	impl_name: create_runtime_str!("muse"),
 	authoring_version: 1,
-	spec_version: 1001,
+	spec_version: 1002,
 	impl_version: 0,
 	apis: RUNTIME_API_VERSIONS,
 	transaction_version: 1,
