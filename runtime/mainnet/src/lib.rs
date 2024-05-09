6e7df1622c565406f3e5f10060023f537d85d39d
metricaez <emiliano@blockdeep.io>
2024-05-09 09:15:36 -0300

    choir: revert spec_ver bump

--- runtime/mainnet/src/lib.rs
+++ runtime/mainnet/src/lib.rs
@@ -220,7 +220,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
 	spec_name: create_runtime_str!("mythos"),
 	impl_name: create_runtime_str!("mythos"),
 	authoring_version: 1,
-	spec_version: 1002,
+	spec_version: 1001,
 	impl_version: 0,
 	apis: RUNTIME_API_VERSIONS,
 	transaction_version: 1,
