6867b57d74e40a66041576865a4a5dcd1602927f
metricaez <emiliano@blockdeep.io>
2024-05-30 14:31:31 -0300

    choir: cargo fmt

--- runtime/common/src/weights/mod.rs
+++ runtime/common/src/weights/mod.rs
@@ -1,9 +1,9 @@
 //! Expose the auto generated weight files.
 
-pub mod pallet_balances;
-pub mod pallet_collator_selection;
 pub mod cumulus_pallet_xcmp_queue;
 pub mod frame_system;
+pub mod pallet_balances;
+pub mod pallet_collator_selection;
 pub mod pallet_escrow;
 pub mod pallet_marketplace;
 pub mod pallet_message_queue;
