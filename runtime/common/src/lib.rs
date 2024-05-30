a9bdd55c8d50e31059f321f25962a6f6805f56cc
metricaez <emiliano@blockdeep.io>
2024-05-30 10:32:25 -0300

    feat: expose weights

--- runtime/common/src/lib.rs
+++ runtime/common/src/lib.rs
@@ -17,6 +17,7 @@ pub use account::EthereumSignature;
 use frame_support::traits::{Currency, Imbalance, Incrementable, OnUnbalanced};
 use pallet_balances::NegativeImbalance;
 use sp_std::marker::PhantomData;
+pub mod weights;
 
 // Cumulus types re-export
 // These types are shared between the mainnet and testnet runtimes
---
+++ runtime/common/src/weights/mod.rs
@@ -0,0 +1,13 @@
+//! Expose the auto generated weight files.
+
+pub mod pallet_balances;
+pub mod pallet_collator_selection;
+pub mod cumulus_pallet_xcmp_queue;
+pub mod frame_system;
+pub mod pallet_escrow;
+pub mod pallet_marketplace;
+pub mod pallet_message_queue;
+pub mod pallet_multisig;
+pub mod pallet_nfts;
+pub mod pallet_session;
+pub mod pallet_timestamp;
