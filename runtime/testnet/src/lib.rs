62c5de2e11a07808f01aa61b48b4ec3876edb608
metricaez <emiliano@blockdeep.io>
2024-05-30 13:06:47 -0300

    feat: impl pallet-collator generated weights

--- runtime/testnet/src/lib.rs
+++ runtime/testnet/src/lib.rs
@@ -574,7 +574,7 @@ impl pallet_collator_selection::Config for Runtime {
 	type ValidatorId = <Self as frame_system::Config>::AccountId;
 	type ValidatorIdOf = pallet_collator_selection::IdentityCollator;
 	type ValidatorRegistration = Session;
-	type WeightInfo = ();
+	type WeightInfo = runtime_common::weights::pallet_collator_selection::WeightInfo<Runtime>;
 }
 
 // Project specific pallets.
