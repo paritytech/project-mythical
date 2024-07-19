2dbc1358c23ad2d1ec1d708941123b6bd9414729
José Molina <jose@blockdeep.io>
2024-07-19 14:28:02 +0200

    Increase spec versions for both runtimes

--- runtime/testnet/src/lib.rs
+++ runtime/testnet/src/lib.rs
@@ -224,7 +224,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
 	spec_name: create_runtime_str!("muse"),
 	impl_name: create_runtime_str!("muse"),
 	authoring_version: 1,
-	spec_version: 1007,
+	spec_version: 1008,
 	impl_version: 0,
 	apis: RUNTIME_API_VERSIONS,
 	transaction_version: 1,
