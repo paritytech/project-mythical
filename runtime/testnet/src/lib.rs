b9e6b50ce334d02842dac017e31179452aecaf92
Valentin Fernandez <tinchofernandez8@gmail.com>
2024-08-15 09:28:56 -0300

    Increase spec-version

--- runtime/testnet/src/lib.rs
+++ runtime/testnet/src/lib.rs
@@ -226,7 +226,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
 	spec_name: create_runtime_str!("muse"),
 	impl_name: create_runtime_str!("muse"),
 	authoring_version: 1,
-	spec_version: 1012,
+	spec_version: 1013,
 	impl_version: 0,
 	apis: RUNTIME_API_VERSIONS,
 	transaction_version: 1,
