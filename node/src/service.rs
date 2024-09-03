d6410f76e5258e33595ca61222e06b390c46c555
metricaez <emiliano@blockdeep.io>
2024-09-03 17:41:13 -0300

    fix: delete unused parameters

--- node/src/service.rs
+++ node/src/service.rs
@@ -540,7 +540,6 @@ where
 		code_hash_provider: move |block_hash| {
 			client.code_at(block_hash).ok().map(ValidationCode).map(|c| c.hash())
 		},
-		sync_oracle,
 		keystore,
 		collator_key,
 		para_id,
@@ -552,10 +551,9 @@ where
 		reinitialize: false,
 	};
 
-	let fut =
-		aura::run::<Block, sp_consensus_aura::sr25519::AuthorityPair, _, _, _, _, _, _, _, _, _>(
-			params,
-		);
+	let fut = aura::run::<Block, sp_consensus_aura::sr25519::AuthorityPair, _, _, _, _, _, _, _, _>(
+		params,
+	);
 	task_manager.spawn_essential_handle().spawn("aura", None, fut);
 
 	Ok(())
