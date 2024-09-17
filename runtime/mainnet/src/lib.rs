837c2a29eabea2b6ca1e334871c2b0bd4fa5db34
José Molina <jose@blockdeep.io>
2024-09-17 17:06:35 +0200

    Increment spec version on mainnet

--- runtime/mainnet/src/lib.rs
+++ runtime/mainnet/src/lib.rs
@@ -234,7 +234,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
 	spec_name: create_runtime_str!("mythos"),
 	impl_name: create_runtime_str!("mythos"),
 	authoring_version: 1,
-	spec_version: 1009,
+	spec_version: 1010,
 	impl_version: 0,
 	apis: RUNTIME_API_VERSIONS,
 	transaction_version: 1,
