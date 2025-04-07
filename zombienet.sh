5d99ce6c16ae56d356ec679855aa61a67fbfc8d9
José Molina <jose@blockdeep.io>
2025-04-07 14:58:30 +0200

    Improve zombienet script

--- zombienet.sh
+++ zombienet.sh
@@ -6,7 +6,7 @@ ZOMBIENET_V=v1.3.128
 POLKADOT_V=stable2503-rc1
 POLKADOT_RUNTIMES_V=v1.4.2
 PASEO_RUNTIMES_V=v1.4.1
-BIN_DIR=bin
+BIN_DIR=./bin
 
 case "$(uname -s)" in
 Linux*) MACHINE=Linux ;;
@@ -107,28 +107,28 @@ zombienet_testnet() {
   zombienet_init
   cargo +stable build --release --features testnet-runtime/metadata-hash
   echo "spawning paseo-local relay chain plus mythos testnet as a parachain..."
-  ./$ZOMBIENET_BIN spawn zombienet-config/testnet.toml -p native
+  $ZOMBIENET_BIN spawn zombienet-config/testnet.toml -p native
 }
 
 zombienet_testnet_asset_hub() {
   zombienet_init
   cargo +stable build --release --features testnet-runtime/metadata-hash
   echo "spawning paseo-local relay chain plus muse testnet as a parachain plus asset-hub..."
-  ./$ZOMBIENET_BIN spawn zombienet-config/testnet-asset-hub.toml -p native
+  $ZOMBIENET_BIN spawn zombienet-config/testnet-asset-hub.toml -p native
 }
 
 zombienet_mainnet() {
   zombienet_init
   cargo +stable build --release --features mainnet-runtime/metadata-hash
   echo "spawning paseo-local relay chain plus mythos mainnet as a parachain..."
-  ./$ZOMBIENET_BIN spawn zombienet-config/mainnet.toml -p native
+  $ZOMBIENET_BIN spawn zombienet-config/mainnet.toml -p native
 }
 
 zombienet_mainnet_asset_hub() {
   zombienet_init
   cargo +stable build --release --features mainnet-runtime/metadata-hash
   echo "spawning polkadot-local relay chain plus mythos mainnet as a parachain plus asset-hub..."
-  ./$ZOMBIENET_BIN spawn zombienet-config/mainnet-asset-hub.toml -p native
+  $ZOMBIENET_BIN spawn zombienet-config/mainnet-asset-hub.toml -p native
 }
 
 print_help() {
