6b0f5af41cdec344319fa72f82ac0b00339c2bc2
José Molina <jose@blockdeep.io>
2025-04-21 08:22:41 +0200

    Increase spec version

--- runtime/mainnet/src/lib.rs
+++ runtime/mainnet/src/lib.rs
@@ -277,7 +277,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
 	spec_name: alloc::borrow::Cow::Borrowed("mythos"),
 	impl_name: alloc::borrow::Cow::Borrowed("mythos"),
 	authoring_version: 1,
-	spec_version: 1014,
+	spec_version: 1015,
 	impl_version: 0,
 	apis: RUNTIME_API_VERSIONS,
 	transaction_version: 1,
