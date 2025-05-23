0b6eff6b5491855d281716963fedc8474c455d22
Valentin Fernandez <tinchofernandez8@gmail.com>
2025-05-23 17:08:02 -0300

    increase spec version

--- runtime/testnet/src/lib.rs
+++ runtime/testnet/src/lib.rs
@@ -288,7 +288,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
 	spec_name: alloc::borrow::Cow::Borrowed("muse"),
 	impl_name: alloc::borrow::Cow::Borrowed("muse"),
 	authoring_version: 1,
-	spec_version: 1027,
+	spec_version: 1028,
 	impl_version: 0,
 	apis: RUNTIME_API_VERSIONS,
 	transaction_version: 1,
