283fd3ec979302a2cfde8b425a3b461fd85f38b3
Mikola Samardak <niksaak@gmail.com>
2026-03-19 14:14:41 +0000

    Increase spec version

--- runtime/mainnet/src/lib.rs
+++ runtime/mainnet/src/lib.rs
@@ -282,7 +282,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
 	spec_name: alloc::borrow::Cow::Borrowed("mythos"),
 	impl_name: alloc::borrow::Cow::Borrowed("mythos"),
 	authoring_version: 1,
-	spec_version: 1020,
+	spec_version: 1021,
 	impl_version: 0,
 	apis: RUNTIME_API_VERSIONS,
 	transaction_version: 1,
